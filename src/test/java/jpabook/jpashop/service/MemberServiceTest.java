7f0127f537c3c68643dfa49a4f4f30b7db417267
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-12-17 02:17:05 +0900

    MemberService Test 클래스 생성

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    public void 회원가입() throws Exception{
+
+    }
+
+    @Test
+    public void 중복_회원_예외()throws Exception{
+
+    }
+}
