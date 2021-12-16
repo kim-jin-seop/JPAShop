0812d85499b5e3f61f25568215891faf2d1d5882
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-12-16 18:28:12 +0900

    code

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; //주문의 상태
+
+    //== 연관관계 편의 메소드==//
+    /*
+    양방향으로 연결해주기 위한 메소드 -> 편의를 위해 두곳에서 add 해주는 작업을 한 곳에서 처리하는 메소드로 제작
+    대체적으로 두 곳 중에서 Control하는 쪽에 생성
+    **/
+
+    //member와 order는 양방향임 따라서 member를 넣어줄 때, 반드시 그 member의 order에도 넣어줌.
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+}
