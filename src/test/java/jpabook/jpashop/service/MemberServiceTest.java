3184a63add5cd0183c25a30adf2eed9599394d8b
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-12-17 03:40:21 +0900

    MemberService Test 구현

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,18 +1,52 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 class MemberServiceTest {
 
-    @Test
-    public void 회원가입() throws Exception{
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
 
+    @Test
+    // @Rollback(value = false)  기DB에서 확인하는 방법
+    public void 회원가입() throws Exception{
+        Member member = new Member();
+        member.setName("member1");
+
+        Long saveId = memberService.join(member);
+
+        em.flush();
+        Assertions.assertThat(member).isEqualTo(memberRepository.findOne(saveId));
     }
 
     @Test
     public void 중복_회원_예외()throws Exception{
+        Member member1 = new Member();
+        member1.setName("member");
 
+        Member member2 = new Member();
+        member2.setName("member");
+
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class,()-> {
+            memberService.join(member2);
+                });//  예외 발생
     }
 }
