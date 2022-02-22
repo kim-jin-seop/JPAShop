b10460ecada6bf6fbbf1ced8a5632cf6e1f83376
kim-jin-seop <tjq2702@naver.com>
2022-02-23 01:12:27 +0900

    조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -46,7 +47,38 @@ public class MemberApiController {
         return new UpdateMemberResponse(id,findMember.getName());
     }
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
 
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getId(),m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(),collect);
+    }
+
+    /**
+     * 유연성을 주기위해 한번 감싸서 보낸다.
+     * @param <T>
+     */
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        Long id;
+        String name;
+    }
 
     @Data
     @AllArgsConstructor
@@ -59,8 +91,6 @@ public class MemberApiController {
     static class UpdateMemberRequest{
         private String name;
     }
-
-
 
     @Data
     static class CreateMemberRequset{
